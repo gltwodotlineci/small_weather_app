197cd3320cce6aef6bd2dc2e6b5a3f0f00d09241
glen <glen84_dr@hotmail.com>
2023-10-06 02:40:56 +0400

    starting app

--- weather_api/weather_backend/views.py
+++ weather_api/weather_backend/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+import datetime
+import requests
 
-# Create your views here.
+def home(request):
+    #api_key = open("/home/glen/Documents/p_doc/projects/weather_proj/weather/weather_api/weather_backend/API_KEY_WEATHER", "r").read()
+    api_key = '31ff1ed1c68f4655b5b200134230510'
+    town = "Tirana"
+    # creating the url with the link + api + town
+    url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={town}"
+    return render(request, 'base/home.html', {})
