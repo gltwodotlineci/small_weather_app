3aedee652e8a3671d614052106a04a2cc6c271b9
glen <glen84_dr@hotmail.com>
2023-10-24 19:21:48 +0400

    serializing the choice and showing api rezults

--- weather_api/weather_backend/serializers.py
+++ weather_api/weather_backend/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
 
 class CitydayValidator(serializers.Serializer):
-    city1 = serializers.CharField(default=True)
-    first_choice_day = serializers.IntegerField(default=True, max_value=7, min_value=0)
+    city1 = serializers.CharField(required=True)
+    day = serializers.IntegerField(required=True)
 
+    #Validating the given day
+    def validate_day(self,value):
+        if value > 7 or value < 1:
+            raise serializers.ValidationError(f"L'utilisateur n'existe pas.")
+
+        return value - 1
--- weather_api/weather_backend/views.py
+++ weather_api/weather_backend/views.py
@@ -1,6 +1,12 @@
+from django.conf import settings
 from django.shortcuts import render
+from django.utils import timezone
+
+from dateutil.relativedelta import relativedelta
+from .serializers import CitydayValidator
+
 from .models import Configuration
-import requests
+import requests, datetime
 #from .serializers import CitydayValidator
 from django.conf import settings
 
@@ -21,18 +27,28 @@ def home(request):
     #If the data are validated:
     validated_data = city_date_validator.validated_data
     '''
-    city1, day = "Paris", 0
+    city1, added_days, day = "Paris", 0, 0
 
     #Geting the data from post
     if request.method == "POST":
-        city1 = request.POST['city1']
-        day = request.POST['first_choice_day']
+
+        city_day_validator = CitydayValidator(data=request.POST)
+        if not city_day_validator.is_valid():
+            #if the data sent are not validated from the serializer
+            print("nnnnnnnnnnnn oooo ooo nn")
+            return render(request,'base/home0.html',{
+                'message': f"Enter the right city name!"
+            })
 
 
-
+        city1 = city_day_validator.validated_data['city1']
+        added_days = city_day_validator.validated_data['day']
     # creating the url with the link + api + town
     #url_data = f"http://api.weatherapi.com/v1/current.json?key=31ff1ed1c68f4655b5b200134230510&q=Paris&days=1&aqi=no&alerts=no"
     url_data = f'http://api.weatherapi.com/v1/forecast.json?key={api_key}&q={city1}&days={day}&aqi=no&alerts=no'
+    day_choosed = timezone.localdate() + relativedelta(days=added_days)
+
     response = requests.get(url_data).json()#.format(api_key, town, day)).json()
+    response['date_chosed'] = day_choosed
 
     return render(request, 'base/home0.html', {'response': response})
